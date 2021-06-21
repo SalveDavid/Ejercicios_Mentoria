2ca556f3d5daf2144bc301051946022fb26258db
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-06-21 00:52:13 -0300

    Ejercicio Completado

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -18,3 +18,59 @@
 #    - position
 #    - the department what they belong
 # - Add some tests to validate this functionality
+
+class Person:
+
+    contador = 0
+
+    def __init__(self, name, lastname, age, address):
+        self.__name = name
+        self.__lastname = lastname
+        self.age = age
+        self.address = address
+        Person.contador += 1
+
+    def fullname(self):
+        return f"{self.__name} {self.__lastname}"
+
+    def without_age(self):
+        return f"{self.__name} {self.__lastname} vive en {self.address}"
+
+
+class Employee(Person):
+
+    def __init__(self, name, lastname, age, address, employee_code, salary_per_hour, start_date, position, department):
+        super().__init__(name, lastname, age, address)
+        self.employee_code = employee_code
+        self.salary_per_hour = salary_per_hour
+        self.start_date = start_date
+        self.position = position
+        self.department = department
+
+    def empleado_completo(self):
+        return f"{self.fullname()} tiene {self.age} años, vive en {self.address}, \n" \
+               f"Su código de empleado es {self.employee_code}, \n" \
+               f"Su salario en de {self.salary_per_hour} dolares y empezó en la fecha {self.start_date}, \n" \
+               f"Su posición es {self.position} y está en el departamento {self.department}"
+
+
+persona1 = Person("Pepe", "Perez", 30, "Caracas")
+persona2 = Person("Misifu", "Godinez", 40, "Rosario")
+persona3 = Person("Techno", "Mantech", 35, "Miami")
+persona4 = Person("Silvester", "Stallone", 75, "Los Angeles")
+persona5 = Person("Keanu", "Reves", 55, "New York")
+persona6 = Person("Jack", "Nicholson", 78, "Who knows")
+
+empleado1 = Employee("Pepito", "Peruño", 40, "Barquisimeto", 101, 2500, "24-06-2021", "Programador", "IT")
+empleado2 = Employee("Masafa", "Dineza", 46, "Toronto", 102, 2000, "21-07-2021", "Arquitecto", "Arquitectura")
+empleado3 = Employee("Paulina", "Rubio", 53, "Cancun", 103, 3500, "12-03-2021", "Gerente", "Gerencia")
+
+
+print(empleado3.empleado_completo())
+# print(persona5.without_age())
+# print(Person.contador, "Instancias de Person han sido creadas")
+# print(persona2.fullname())
+# print(persona1.address)
+# print(persona3.age)
+
+
