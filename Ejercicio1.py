e66b47a0fffb19da8ba23a9c63fc9265188aeaa6
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-06-20 23:12:06 -0300

    Copiar enunciado - first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,20 @@
+# Create a class Person with te attributes:
+# - name
+# - lastname
+# - age
+# - address
+#
+# Functionality:
+# - The attributes are mandatory to create an object from this class.
+# - The name and lastname should be private.
+# - Add a method to return the person's fullname.
+# - Create some instances of this class as an example.
+# - Add a method to this class in order to return an instance of Person without the age.
+# - Add a variable to the class that allow us to know how many instances of Person has been created.
+# - Create a class Employee that inherits the behaviors of Person. Include the following attributes:
+#    - employee code
+#    - salary per hour
+#    - start date
+#    - position
+#    - the department what they belong
+# - Add some tests to validate this functionality
