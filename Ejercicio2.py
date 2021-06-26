ec94e6092a09c38c29e949619a1324a4e19437f4
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-06-26 18:17:08 -0300

    Aparición de menor sueldo con código más limpio, Cuarto Commit

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -29,35 +29,24 @@ sueldos2 = []
 
 with open('empleados.txt') as file:
     lista_con_punto_y_coma = [con_punto_y_coma.rstrip() for con_punto_y_coma in file]
-#print(lista_con_punto_y_coma)
 
 for con_punto_y_coma in lista_con_punto_y_coma:
     lista_de_listas_con_coma.append(con_punto_y_coma.split(";"))
-#print(lista_de_listas_con_coma)
 
 for i in range(1, len(lista_de_listas_con_coma)):
     lista_de_listas_con_coma_sin_primeralinea.append(lista_de_listas_con_coma[i])
-#print(lista_de_listas_con_coma_sin_primeralinea)
 
 for colum in lista_de_listas_con_coma_sin_primeralinea:
     del colum[0]
     lista_de_listas_con_coma_sin_primeracolumna = lista_de_listas_con_coma_sin_primeralinea
-#print(lista_de_listas_con_coma_sin_primeracolumna)
-
 
 for i in lista_de_listas_con_coma_sin_primeracolumna:
     if i not in lista_de_listas_con_coma_sin_repeticiones:
         lista_de_listas_con_coma_sin_repeticiones.append(i)
-print(lista_de_listas_con_coma_sin_repeticiones)
 
-
-# for i in range(len(lista_de_listas_con_coma_sin_repeticiones)):
-#     for j in range(5, 6):
-#         sueldos.append(lista_de_listas_con_coma_sin_repeticiones[i][j])
 
 sueldos2 = [sueldos.append(lista_de_listas_con_coma_sin_repeticiones[i][j]) for j in range(5, 6) for i in
             range(len(lista_de_listas_con_coma_sin_repeticiones))]
-print(sueldos)
 
 for i in range(len(sueldos)-1):
     if sueldos[i] < sueldos[i + 1]:
