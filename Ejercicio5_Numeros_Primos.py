ae80871c44a9723ee0aefa6ed6b4171800d7cc0d
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-07-05 20:49:56 -0300

    Primer Commit

--- Ejercicio5_Numeros_Primos.py
+++ Ejercicio5_Numeros_Primos.py
@@ -1,1 +1,14 @@
-numeros primos y generadores
+# ENCONTRAR NUMEROS PRIMOS DE 1 A 100.
+
+for n in range(1, 101):
+    cant_divisores = 0
+    i = 1
+    while i <= n:
+        if n % i == 0:
+            cant_divisores += 1
+        i += 1
+    if cant_divisores == 2:
+        print(f"El número {n} es primo")
+    else:
+        print(f"El número {n} NO es primo")
+
