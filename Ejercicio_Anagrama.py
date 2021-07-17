1e8d2b51d273c156721138a112dddc5b869574ae
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-07-17 16:03:34 -0300

    Ejercicio_Anagrama.py

--- Ejercicio_Anagrama.py
+++ Ejercicio_Anagrama.py
@@ -5,7 +5,8 @@
 # anim al a n imal
 # lamin a l amina
 
-def ordenamiento(lista):
+def ordenamiento(texto):
+    lista = list(texto)
     for i in range(len(lista) - 1):
         for j in range(len(lista) - 1):
             if lista[j] > lista[j + 1]:
@@ -16,17 +17,8 @@ def ordenamiento(lista):
 
 
 def anagrama(texto1, texto2):
-    texto1 = texto1.lower()
-    texto2 = texto2.lower()
 
-    lista1 = list(texto1)
-    lista2 = list(texto2)
-
-    texto1_ordenado = "".join(ordenamiento(lista1)).replace(" ", "")
-
-    texto2_ordenado = "".join(ordenamiento(lista2)).replace(" ", "")
-
-    return texto1_ordenado == texto2_ordenado
+    return "".join(ordenamiento(texto1)) == "".join(ordenamiento(texto2))
 
 
-print(anagrama(input("Ingrese cadena1: "), input("Ingrese cadena2: ")))
+print(anagrama(input("Ingrese cadena1: ").replace(" ", "").lower(), input("Ingrese cadena2: ").replace(" ", "").lower()))
