88f198fc01a4b05a4ff2e56ae773a614d4af1d5e
David Augusto Fuenmayor Martinez <david.fm@globant.com>
2021-07-20 00:22:29 -0300

    Primer commit palindromo

---
+++ Palindromo.py
@@ -0,0 +1,13 @@
+# Encontrar si una cadena de texto es palindromo.
+# Consideraciones: puede ser palabra, oracion.
+# Crear funcion, solo utiliza 1 cadena de texto
+
+
+def palindromo(texto):
+
+    texto = texto.lower()
+
+    return texto == texto[::-1]
+
+
+print(palindromo(input("Ingrese palabra u oración: ").replace(" ", "")))
